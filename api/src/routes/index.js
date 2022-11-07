e2564f8ce4b342013009766180af58a225bdc7e6
gabrielCarrenio <carrenocarlosgabriel@gmail.com>
2022-11-07 20:43:59 -0300

    taking a test

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,11 +2,8 @@ const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
-
 const router = Router();
 
-// Configurar los routers
-// Ejemplo: router.use('/auth', authRouter);
-
+router.get('/', async (req, res) => console.log('test'));
 
 module.exports = router;
