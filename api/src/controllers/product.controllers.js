4bf0b963c18c4f0684ee35a4c96e0ebdbbb7de3b
Micaela Ceballos <micaelaceballos411@gmail.com>
2022-11-08 18:58:09 -0300

    Products controller returns all the data

---
+++ api/src/controllers/product.controllers.js
@@ -0,0 +1,14 @@
+const { response, request } = require("express");
+const { getProductsByJson } = require("../helpers/getProductsByJson");
+const getProducts = (req = request, res = response) => {
+  try {
+    const products = getProductsByJson();
+    res.status(200).send(products);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
+module.exports = {
+  getProducts,
+};
