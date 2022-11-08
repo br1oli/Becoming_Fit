d9d69859cb25a18ddb75b265b0ae586ef13f8845
Jessica <jessicafrancosantamaria@gmail.com>
2022-11-08 14:58:38 -0500

    Feat: Sketch for a search input

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import LandingPage from "./COMPONENTS/LandingPage";
+import Search from './COMPONENTS/LandingPage/Search';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={LandingPage} />
+        <Route exact path="/home" component={Search} />
       </Switch>
     </BrowserRouter>
   );
