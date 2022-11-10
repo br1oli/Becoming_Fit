440194927264d8e1b93b1a4ab266bf8b8aea771a
br1oli <joaquinolivera_2000@hotmail.com>
2022-11-10 18:55:33 -0300

    solución reducer 2

--- api/src/db.js
+++ api/src/db.js
@@ -2,9 +2,9 @@ require("dotenv").config();
 const { Sequelize, Op } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-// const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
-const { DB_USER, DB_PASSWORD, DB_HOST } = require('../.env');
-require('../.env')
+const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+// const { DB_USER, DB_PASSWORD, DB_HOST } = require('../.env');
+
 
 const sequelize = new Sequelize(
   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/becomingfit`,
