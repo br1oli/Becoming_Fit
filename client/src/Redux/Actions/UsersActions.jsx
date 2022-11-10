5d98e0278e62878c41f0917b06c2ec3b077e276c
br1oli <joaquinolivera_2000@hotmail.com>
2022-11-10 17:29:19 -0300

    avance cartas en home

---
+++ client/src/Redux/Actions/UsersActions.jsx
@@ -0,0 +1,19 @@
+import axios from 'axios'
+
+export  function getAllProducts(payload){
+    return async function(dispath){
+        try{
+            var json = await axios.get(`http://localhost:3001/products`);
+            return dispath({
+                type: "GET_PRODUCTS",
+                payload: json.data
+            })
+        }catch(error){
+            console.log(error, "error al traer los productos")
+            return dispath({
+                type: "ERROR",
+                payload: error
+            })
+        }
+    }
+}
