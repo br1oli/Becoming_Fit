1ebfb29f56bafba7b9754045ed8a7c95e16bd3b1
Micaela Ceballos <micaelaceballos411@gmail.com>
2022-11-10 00:58:03 -0300

    foundByName functionality added

--- api/src/controllers/product.controllers.js
+++ api/src/controllers/product.controllers.js
@@ -2,9 +2,18 @@ const { response, request } = require("express");
 const getProductsFromDb = require("../helpers/getProducts");
 const getProducts = async (req = request, res = response) => {
   try {
+    let { name } = req.query;
     const products = await getProductsFromDb();
     if (!products.length) {
       return res.status(404).send("No products found");
+    }
+
+    if (name) {
+      let foundProductByName = products.filter((p) =>
+        p.name.toLowerCase().includes(name.toLowerCase())
+      );
+
+      return res.status(200).send(foundProductByName);
     }
     res.status(200).send(products);
   } catch (error) {
