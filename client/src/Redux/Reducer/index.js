89ec9259ce3d933ea0faa8f9b5d3e7424325c000
br1oli <joaquinolivera_2000@hotmail.com>
2022-11-10 18:49:45 -0300

    Solución reducer

---
+++ client/src/Redux/Reducer/index.js
@@ -0,0 +1,35 @@
+import { combineReducers } from 'redux';
+import { products } from './products';
+import { detailProduct } from './detailProduct';
+import { brands } from './brands';
+
+const reducer = combineReducers({
+  products,
+  detailProduct,
+  brands,
+});
+
+export default reducer;
+
+// const initialState = {
+//   porducts: [],
+//   allProducts: [],
+//   brands: [],
+//   allBrands: [],
+//   details: [],
+// };
+
+// function rootReducer(state = initialState, action) {
+//   switch (action.type) {
+//     case "GET_PRODUCTS":
+//       return {
+//         ...state,
+//         porducts: action.payload,
+//         allProducts: action.payload,
+//       };
+//     default:
+//       return state;
+//   }
+// }
+
+// export default rootReducer;
