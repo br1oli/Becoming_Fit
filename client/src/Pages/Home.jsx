9eca9244662c8e66c9b36ae4df6f29ca43c0e929
Braian Veron <braianveron88@gmail.com>
2022-11-11 00:31:27 -0300

    Merge branch 'dev' of https://github.com/jessicafrancos/Becoming_Fit into BV_branch

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import NavBar from "../Components/NavBar";
 import Footer from "../Components/Footer";
 import "../Components/Style/Home.css";
+import Filters from "../Components/Filters";
 
 const Home = () => {
   return (
     <div className="home-container">
       <div className="nav.container">
         <NavBar />
+      </div>
+      <div className="filters-container">
+        <Filters />
       </div>
       <div className="cards-container">
         <h2>CARDS</h2>
