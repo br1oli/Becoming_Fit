3cd3798c2e59c3efb1953a57f657dc206deb0f1f
Micaela Ceballos <83962558+MicaCblls@users.noreply.github.com>
2022-11-11 01:08:22 -0300

    Update Actions.js

--- client/src/Redux/Actions/Actions.js
+++ client/src/Redux/Actions/Actions.js
@@ -14,13 +14,11 @@ export  function getAllProducts(payload){
     return async function(dispath){
         try{
             var json = await axios.get(`http://localhost:3001/products`);
-            console.log(json.data, "SOY JSON")
             return dispath({
                 type: 'GET_PRODUCTS',
                 payload: json.data
             })
         }catch(error){
-            console.log(error, "error al traer los productos")
             return dispath({
                 type: "ERROR",
                 payload: error
