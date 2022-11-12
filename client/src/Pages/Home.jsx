3d8744c93686b8fea044b9525bb3bbcf6c49bb4f
Micaela Ceballos <micaelaceballos411@gmail.com>
2022-11-11 21:51:27 -0300

    fix import

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,22 +1,23 @@
 import React, { useEffect } from "react";
 import NavBar from "../Components/NavBar";
 import Footer from "../Components/Footer";
+
 //import "../Components/Style/Home.css";
 import Filters from "../Components/FIlters";
 import Styles from "../Components/Style/Home.module.css";
 import { useDispatch, useSelector } from "react-redux";
 import ProductCard from "../Components/ProductCardsindex";
 import { getAllProducts } from "../Redux/Actions/Actions";
-import { products } from "../Redux/Reducer/products";
+import { NavLink } from "react-router-dom";
 
 const Home = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getAllProducts());
-  }, [dispatch]);
+  }, []);
 
-  const productos = useSelector((state) => state.products);
+  const productos = useSelector((state) => state.allProducts);
 
   return (
     <div className={Styles.homeContainer}>
@@ -33,20 +34,29 @@ const Home = () => {
               console.log(productos, "Products Home");
               return (
                 <div>
-                  <ProductCard
-                    name={p.name}
-                    id={p.id}
-                    image={p.image}
-                    brand={p.brandName}
-                    price={p.price}
-                  />
+                  <NavLink
+                    //  style={{
+                    //     textDecoration: 'none',
+                    //     textDecorationColor: "black",
+                    //     color: 'black'
+                    // }}
+                    to={`/home/${p.id}`}
+                  >
+                    <ProductCard
+                      name={p.name}
+                      id={p.id}
+                      image={p.image}
+                      brand={p.brandName}
+                      price={p.price}
+                    />
+                  </NavLink>
                 </div>
               );
             })}
         </div>
       </div>
       <div className="footer-container"></div>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 };
