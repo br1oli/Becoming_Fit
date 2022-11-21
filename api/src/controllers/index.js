ae2f6d88e3b3df3c70b68b9c2d83c1e43c81efd1
Micaela Ceballos <micaelaceballos411@gmail.com>
2022-11-21 20:47:06 -0300

    centralizing controlers

--- api/src/controllers/index.js
+++ api/src/controllers/index.js
@@ -4,8 +4,15 @@ const {
   deleteUser,
   updateUser,
 } = require("./users.controllers");
+const { getCart,postProductToCart , deleteCart, deleteCartProduct}= require('./cart.controllers')
 const { getOrders } = require("./order.controllers");
-const { getProducts } = require("./product.controllers");
+const {
+  getProducts,
+  createProduct,
+  updateProduct,
+  deleteProductFromDb,
+} = require("./product.controllers");
+const {getDetail}= require('./detail.controller')
 const {
   getCategories,
   createCategory,
@@ -19,7 +26,12 @@ module.exports = {
   updateUser,
   getOrders,
   getProducts,
+  createProduct,
+  updateProduct,
+  deleteProductFromDb,
+  getDetail,
   getCategories,
   createCategory,
   deleteCategory,
+  getCart,postProductToCart , deleteCart, deleteCartProduct
 };
