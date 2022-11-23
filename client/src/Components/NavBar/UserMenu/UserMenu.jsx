20d733b1d970a52185482aaf445b585cdfdc9aec
Micaela Ceballos <83962558+MicaCblls@users.noreply.github.com>
2022-11-23 00:00:45 -0300

    missing link

--- client/src/Components/NavBar/UserMenu/UserMenu.jsx
+++ client/src/Components/NavBar/UserMenu/UserMenu.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 import Offcanvas from "react-bootstrap/Offcanvas";
 import { HiUserCircle } from "react-icons/hi";
