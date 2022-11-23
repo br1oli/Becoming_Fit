ae105184586cb45b626c1b413faae4df48f961ad
Micaela Ceballos <83962558+MicaCblls@users.noreply.github.com>
2022-11-22 23:51:41 -0300

    Fixing PORT

--- api/src/db.js
+++ api/src/db.js
@@ -10,7 +10,7 @@ let sequelize =
         database: DB_NAME,
         dialect: "postgres",
         host: DB_HOST,
-        port: PORT,
+        port: 6965,
         username: DB_USER,
         password: DB_PASSWORD,
         pool: {
