e0babeb58c89261f870b7e76e226ff03d7762614
AntilopeDisecado <IvanConsorte@yahoo.com.ar>
2022-11-27 12:21:15 -0300

    'pulling from dev'

--- client/src/Components/Form/Form.jsx
+++ client/src/Components/Form/Form.jsx
@@ -4,9 +4,9 @@ import { Button, Form } from 'react-bootstrap';
 import { useHistory } from 'react-router-dom';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { useSelector } from 'react-redux';
-import Loading from "../../Utils/Loading.gif";
 import { createUserProfile } from '../../Redux/Actions/UsersActions';
+import { useSelector } from 'react-redux';
+
 const FormComplete = () => {
     const usuarios = useSelector((state) => state.userProfile)
     const history = useHistory()
@@ -26,30 +26,32 @@ const FormComplete = () => {
         adress: "",
     });
 
-  useEffect(() => {
-    const relleno = async () => {
-      try {
-        if (isAuthenticated) {
-          console.log("AHORA SI ESTOY AUTENTICADO");
-          setInput({
-            ...input,
-            name: user.name,
-            email: user.email,
-            // zipCode: usuarios.zipCode? usuarios.zipCode: "",
-            // country: usuarios.country? usuarios.country: "",
-            // city: usuarios.city? usuarios.city: "",
-            // phone: usuarios.phone? usuarios.phone: "",
-            // adress: usuarios.adress? usuarios.adress: [],
-          });
-        } else {
-          console.log("NO ESTA AUTENTICADO");
+
+
+    useEffect(() => {
+        const relleno = async () => {
+            try {
+                if (isAuthenticated) {
+                    console.log("AHORA SI ESTOY AUTENTICADO")
+                    setInput({
+                        ...input,
+                        name: user.name,
+                        email: user.email,
+                        // zipCode: usuarios.zipCode? usuarios.zipCode: "",
+                        // country: usuarios.country? usuarios.country: "",
+                        // city: usuarios.city? usuarios.city: "",
+                        // phone: usuarios.phone? usuarios.phone: "",
+                        // adress: usuarios.adress? usuarios.adress: [],
+                    })
+                } else {
+                    console.log('NO ESTA AUTENTICADO')
+                }
+            } catch (error) {
+                console.log(error)
+            }
         }
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    relleno();
-  }, [user]);
+        relleno()
+    }, [user])
 
     function validate(input) {
         let errors = {};
