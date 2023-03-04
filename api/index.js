766e0f53b81abfc2b0c727ea47bb08a412f3f013
Micaela Ceballos <micaelaceballos411@gmail.com>
2023-03-04 16:48:06 -0300

    fix enviroment variable

--- api/index.js
+++ api/index.js
@@ -1,12 +1,12 @@
 const { conn } = require("./src/db.js");
 const server = require("./src/app.js");
 const preChargeProducts = require("./src/helpers/preChargeProducts");
-const { PGPORT } = process.env;
+const { PORT } = process.env;
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(async () => {
   await preChargeProducts();
-  server.listen(PGPORT, () => {
-    console.log(`%s listening at ${PGPORT}`); // eslint-disable-line no-console
+  server.listen(PORT, () => {
+    console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
   });
 });
