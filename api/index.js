7a5f4ae5be7123d64b12af42d28b85138c09e9d2
Micaela Ceballos <micaelaceballos411@gmail.com>
2023-03-04 15:20:52 -0300

    try deploy back in railway

--- api/index.js
+++ api/index.js
@@ -1,12 +1,12 @@
 const { conn } = require("./src/db.js");
 const server = require("./src/app.js");
 const preChargeProducts = require("./src/helpers/preChargeProducts");
-const { PORT } = process.env;
+const { PGPORT } = process.env;
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(async () => {
   await preChargeProducts();
-  server.listen(process.env.PORT, () => {
-    console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
+  server.listen(process.env.PGPORT, () => {
+    console.log(`%s listening at ${PGPORT}`); // eslint-disable-line no-console
   });
 });
